30916e0a0baeb3f9e030bb5a44fc72f71d74a12c
Jafar Sherif <jafarsherif27@gmail.com>
2025-02-06 02:23:05 +0530

    ME_QA_XGOOGLE_FORM_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -1,26 +1,90 @@
 package demo;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.logging.Level;
 // import io.github.bonigarcia.wdm.WebDriverManager;
 import demo.wrappers.Wrappers;
+import net.bytebuddy.asm.Advice.Local;
 
+@SuppressWarnings({"unused"})
 public class TestCases {
     ChromeDriver driver;
+    Wrappers wrappers;
+    String expectedMessage = "Thanks for your response, Automation Wizard!";
+    String actualMessage = "";
 
     /*
      * TODO: Write your tests here with testng @Test annotation. 
      * Follow `testCase01` `testCase02`... format or what is provided in instructions
      */
+
+    @Test
+    public void testCase01(){
+        wrappers.logStatus("Start Test Case","testCase01");
+
+        wrappers.navigateToUrl("https://docs.google.com/forms/d/e/1FAIpQLSep9LTMntH5YqIXa5nkiPKSs283kdwitBBhXWyZdAS-e4CxBQ/viewform");
+
+        wrappers.sendKeys(By.xpath("(//input[@class='whsOnd zHQkBf'])[1]"), "Crio Learner");
+
+        
+        System.out.println("epoch: "+wrappers.getEpoch());
+        wrappers.sendKeys(By.tagName("textarea"),String.format("%s %s","I want to be the best QA Engineer!",wrappers.getEpoch()));
+
+        wrappers.clickOnElement(By.xpath("(//div[@class='bzfPab wFGF8'])[1]"));
+
+        wrappers.clickOnElement(By.xpath("(//div[@class='bzfPab wFGF8'])[5]"));
+        wrappers.clickOnElement(By.xpath("(//div[@class='bzfPab wFGF8'])[6]"));
+        wrappers.clickOnElement(By.xpath("(//div[@class='bzfPab wFGF8'])[8]"));
+
+        wrappers.clickOnElement(By.className("DEh1R"));
+        wrappers.clickOnElement(By.xpath("//div[@data-value='Mr' and @role='option']"));
+
+
+        LocalDate currDate = LocalDate.now();
+        LocalDate dateSevenDaysBefore = currDate.minusDays(7);
+
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        System.out.println("Date7daysAgo: "+dateSevenDaysBefore.format(dateFormat));
+        wrappers.sendKeys(By.xpath("//input[@type='date']"),dateSevenDaysBefore.format(dateFormat));
+
+
+        LocalTime currentTime = LocalTime.now();
+        DateTimeFormatter timeFormatH = DateTimeFormatter.ofPattern("HH");
+
+        System.out.println("Hour: "+currentTime.format(timeFormatH));
+        wrappers.sendKeys(By.xpath("(//input[@class='whsOnd zHQkBf'])[3]"),currentTime.format(timeFormatH));
+
+        DateTimeFormatter timeFormatM = DateTimeFormatter.ofPattern("mm");
+
+        System.out.println("Minutes: "+currentTime.format(timeFormatM));
+        wrappers.sendKeys(By.xpath("(//input[@class='whsOnd zHQkBf'])[4]"),currentTime.format(timeFormatM));
+
+        wrappers.clickOnElement(By.xpath("(//span[@class='l4V7wb Fxmcue'])[1]"));
+
+        this.actualMessage = wrappers.getText(By.xpath("//div[@class='vHW8K']"));
+
+        System.out.println("The success message: "+actualMessage);
+
+        Assert.assertEquals(actualMessage, expectedMessage,"Success message mismatched");
+
+        wrappers.logStatus("End Test Case","testCase01");
+    }
+
+
 
      
     /*
@@ -47,6 +111,8 @@ public class TestCases {
         driver = new ChromeDriver(options);
 
         driver.manage().window().maximize();
+
+        this.wrappers = new Wrappers(driver);
     }
 
     @AfterTest
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -6,14 +6,93 @@ import java.time.format.DateTimeFormatter;
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
+@SuppressWarnings({ "unused" })
 public class Wrappers {
     /*
      * Write your selenium wrappers here
      */
+    public WebDriver driver;
+    Actions actions;
+    WebDriverWait wait;
+
+
+    public Wrappers(WebDriver driver) {
+        this.driver = driver;
+        this.actions = new Actions(driver);
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(25));
+    }
+
+
+    public void logStatus(String description, String testID) {
+        String timeStamp = String.valueOf(java.time.LocalDateTime.now());
+        System.out.println(String.format("%s | %s | %s", timeStamp, description, testID));
+
+    }
+
+
+    public void logStatus(String description, boolean status) {
+        String timeStamp = String.valueOf(java.time.LocalDateTime.now());
+        System.out.println(String.format("%s | %s | %s", timeStamp, description, (status ? "Passed" : "Failed")));
+    }
+
+
+    public void sendKeys(By locator, String text) {
+        try {
+            WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+            element.clear();
+            element.sendKeys(text);
+
+        } catch (Exception e) {
+            System.out.println("Failed to enter text in element: " + locator);
+            e.printStackTrace();
+        }
+    }
+
+
+    public void clickOnElement(By locator) {
+        try {
+            WebElement element = wait.until(ExpectedConditions.elementToBeClickable(locator));
+            element.click();
+
+        } catch (Exception e) {
+            System.out.println("Failed to click on element: " + locator);
+            e.printStackTrace();
+        }
+    }
+
+
+    public void navigateToUrl(String url) {
+        driver.get(url);
+    }
+
+
+    public String getText(By locator) {
+        try {
+            WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+            return element.getText();
+
+        } catch (Exception e) {
+            System.out.println("Failed to get text from element: " + locator);
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+
+    public String getEpoch() {
+        long epoch = System.currentTimeMillis() / 1000;
+        return String.valueOf(epoch);
+
+    }
+
 }
